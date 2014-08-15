c05a595dd300843794bfa56f3d754e948fb4bc0d
DDChiang <DDChiang@users.noreply.github.com>
2014-08-14 21:02:42 -0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title>The Oaktree</title>
+		
+	</head>
+	<body>
+			<?php include 'header.php';?>
+			<div class="content-wrapper">
+				<?php include 'navbar.php';?>
+				<?php include 'home-content.php';?>
+				
+			</div>
+	</body>
+	
+</html>
